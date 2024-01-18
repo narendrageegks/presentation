52a85a110650971856e4128682cda72b7a569ea1
narendrageegks <80438367+narendrageegks@users.noreply.github.com>
2024-01-18 18:46:14 +0530

    Add files via upload

---
+++ up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DOCKER_HUB_USERNAME="pmo839"
+IMAGE_NAME="lawn_ai"
+PASS="@jupiter123"
+
+sudo apt-get update
+sudo apt-get install -y docker.io
+
+docker login -u "$DOCKER_HUB_USERNAME" -p "$PASS"
+
+docker pull "$DOCKER_HUB_USERNAME/$IMAGE_NAME:latest"
+if ! docker info | grep -q "Swarm: active"; then
+    sudo docker swarm init
+fi
+docker service rm my-lawn-service 2>/dev/null
+docker service create --name my-lawn-service -p 8000:8000 --replicas 1 "$DOCKER_HUB_USERNAME/$IMAGE_NAME:latest"
