6a2c4a41ea63d9acd6786549bb95a1d42d03b801
narendrageegks <80438367+narendrageegks@users.noreply.github.com>
2024-01-18 18:41:32 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.8-slim
+WORKDIR /app
+COPY . /app
+RUN pip install --upgrade pip
+RUN pip install  -r requirements.txt
+EXPOSE 5000
+ENV FLASK_APP=app.py
+CMD ["flask", "run", "--host=0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_container_info():
+
+    # Get the container ID from the environment (if available)
+    container_id = os.getenv("HOSTNAME")
+
+    return {"data":f"Container ID: {container_id}"}
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
