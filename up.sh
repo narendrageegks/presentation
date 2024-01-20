bddffb6e06fbfd535dcb984098690806b2979767
narendrageegks <80438367+narendrageegks@users.noreply.github.com>
2024-01-20 15:14:30 +0530

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOCKER_HUB_USERNAME="pmo839"
-IMAGE_NAME="lawn_ai"
+IMAGE_NAME="flask"
 PASS="@jupiter123"
 
 sudo apt-get update
@@ -14,4 +14,4 @@ if ! docker info | grep -q "Swarm: active"; then
     sudo docker swarm init
 fi
 docker service rm my-lawn-service 2>/dev/null
-docker service create --name my-lawn-service -p 8000:8000 --replicas 1 "$DOCKER_HUB_USERNAME/$IMAGE_NAME:latest"
+docker service create --name flask-service -p 8000:8000 --replicas 1 "$DOCKER_HUB_USERNAME/$IMAGE_NAME:latest"
